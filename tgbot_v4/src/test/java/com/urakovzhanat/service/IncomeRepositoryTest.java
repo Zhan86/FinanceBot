d209607e17af4abfcfceb57cfe4a94f58b5a5d4e
UrakovZhanat <zhanaturakov8897@gmail.com>
2023-11-04 19:36:46 +0300

    sql data test commit

--- tgbot_v4/src/test/java/com/urakovzhanat/service/IncomeRepositoryTest.java
+++ tgbot_v4/src/test/java/com/urakovzhanat/service/IncomeRepositoryTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +21,13 @@ public class IncomeRepositoryTest {
     public void testRepo() {
         for (int i = 0; i < 10; i++, incomeRepository.save(new Income())) ;
         final List<Income> found = incomeRepository.findAll();
-        assertEquals(10, found.size());
+        assertEquals(11, found.size());
+    }
+
+    @Test
+    public void testDataScripts() {
+        Optional<Income> income = incomeRepository.findById(12345L);
+        assertTrue(income.isPresent());
+        assertEquals(new BigDecimal("3000.00"), income.get().getIncome());
     }
 }
